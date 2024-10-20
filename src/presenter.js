bf66d6ab1d9565ecdd76fd2d874b0b573e4606d2
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-10-19 22:48:04 -0400

    Registrar gasto: Prueba funcionando

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import Presupuesto from "./presupuesto.js";
 import Ingreso from "./ingreso.js";
 
 //Gasto
+const fechaGasto = document.querySelector("#fecha-gasto");
 const montoGasto = document.querySelector("#monto-gasto");
 const form_gasto = document.querySelector("#gastos-form");
 const div_gastos = document.querySelector("#gastos-div");
@@ -24,9 +25,12 @@ form_gasto.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const valor_gasto = Number.parseInt(montoGasto.value);
-  gastito.agregarMonto(valor_gasto);
+  const fecha_gasto = fechaGasto.value; 
 
-  div_gastos.innerHTML = "<p>" + gastito.mostrarMonto() + "</p>";
+  gastito.agregarMonto(valor_gasto);
+  gastito.agregarFecha(fecha_gasto);
+
+  div_gastos.innerHTML = "<p>" + gastito.mostrarMonto() + "<p>" + gastito.mostrarFecha() + "</p>";
 });
 
 form_presupuesto.addEventListener("submit", (event) => {
