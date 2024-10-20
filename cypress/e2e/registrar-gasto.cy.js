0383bca5a7e3e3f7d91575a6c1f88998bcefe19e
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-10-19 23:19:38 -0400

    Registrar gasto con fecha y nota: Error esperado con cypress1

--- cypress/e2e/registrar-gasto.cy.js
+++ cypress/e2e/registrar-gasto.cy.js
@@ -13,4 +13,13 @@ describe("Registrar Gasto", () => {
     cy.get("#aniadir-gasto").click();
     cy.get("#gastos-div").should("contain", "4").and("contain", "2024-10-19");
   });
+
+  it("Muestra monto de gasto ingresado,fecha y nota", () => {
+    cy.visit("/");
+    cy.get("#monto-gasto").type(4);
+    cy.get("#fecha-gasto").type("2024-10-19");
+    cy.get("#nota").type("Gasto en comida");
+    cy.get("#aniadir-gasto").click();
+    cy.get("#gastos-div").should("contain", "4").and("contain", "2024-10-19").and("contain", "Gasto en comida");
+  });
 });
