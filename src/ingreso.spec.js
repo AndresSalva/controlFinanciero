b3518550ed4d9240059df910da33e77262aad167
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-10-20 00:48:28 -0400

    prueba tdd mostrar monto y fecha de aniadir ingreso

--- src/ingreso.spec.js
+++ src/ingreso.spec.js
@@ -6,4 +6,11 @@ describe("Ingresos", () => {
     ingreso.agregarMonto(5)
     expect(ingreso.mostrarMonto()).toEqual('Monto: 5');
   });
+  it("Deberia registrar monto ingreso con fecha", () => {
+    const ingreso = new Ingreso
+    ingreso.agregarMonto(1)
+    ingreso.agregarFecha("2024-10-14")
+    expect(ingreso.mostrarMonto()).toEqual('Monto: 1');
+    expect(ingreso.mostrarFecha()).toEqual('Fecha: 2024-10-14');
+  });
 });
