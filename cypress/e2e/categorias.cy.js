8e296cab0b3fa05fe75cc0ac10555a4793a1a2e3
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-10-19 23:30:56 -0400

    Ver categoria ingresos

--- cypress/e2e/categorias.cy.js
+++ cypress/e2e/categorias.cy.js
@@ -4,5 +4,9 @@ describe("mostrar categorias", () => {
       cy.get("#gastos-btn").click();
       cy.get('#imgGastos').should('be.visible').and('have.prop', 'naturalWidth').should('be.greaterThan', 0)
     });
-    
+    it("Muestra categoria ingresos", () => {
+        cy.visit("/");
+        cy.get("#ingresos-btn").click();
+        cy.get('#imgIngresos').should('be.visible').and('have.prop', 'naturalWidth').should('be.greaterThan', 0)
+    });
   });
