2ad2beeea183431f00e26e0de57393eb64c5bd2d
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-10-20 00:49:03 -0400

    aniadir monto y fecha de ingreso

--- src/ingreso.js
+++ src/ingreso.js
@@ -1,12 +1,19 @@
 class Ingreso {
     constructor() {
       this.monto = null;
+      this.fecha= null;
     }
     agregarMonto(monto) {
       this.monto = monto;
     }
+    agregarFecha(fecha) {
+      this.fecha= fecha;
+    }
     mostrarMonto() {
       return `Monto: ${this.monto}`;
+    }
+    mostrarFecha() {
+      return `Fecha: ${this.fecha}`;
     }
 }
 export default Ingreso;
