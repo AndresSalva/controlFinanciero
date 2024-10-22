e630d865603ab47345d9178bec8973356692ff59
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-10-22 17:58:33 -0400

    pruebas unitarias para ingresar mas de 2 montos en la lista ingresos

--- src/lista-ingresos.spec.js
+++ src/lista-ingresos.spec.js
@@ -1,7 +1,6 @@
 import ListaIngresos from "./lista-ingresos.js";
 import Ingreso from "./ingreso.js";
 
-
 describe("Lista ingreso", () => {
   it("Deberia registrar un ingreso en la lista", () => {
     const ingreso = new Ingreso();
@@ -17,6 +16,31 @@ describe("Lista ingreso", () => {
       }];
     expect(lista.obtenerIngreso()).toEqual(resultado);
   });
-  
+  //nuevo
+  it("Deberia registrar dos o mas ingresos en la lista", () => {
+    const ingreso1 = new Ingreso();
+    ingreso1.agregarMonto(150)
+    ingreso1.agregarFecha("2024-10-12")
+    ingreso1.agregarNota("Salario")
+    const ingreso2 = new Ingreso();
+    ingreso2.agregarMonto(90)
+    ingreso2.agregarFecha("2024-10-10")
+    ingreso2.agregarNota("Regalo")
+    const lista = new ListaIngresos;
+    lista.registrarIngreso(ingreso1);
+    lista.registrarIngreso(ingreso2);
+    const resultado = [{
+        monto: 150,
+        fecha: "2024-10-12",
+        nota: "Salario",
+      },
+      {
+        monto: 90,
+        fecha: "2024-10-10",
+        nota: "Regalo",
+      }
+    ];
+    expect(lista.obtenerIngreso()).toEqual(resultado);
+  });
 
 });
