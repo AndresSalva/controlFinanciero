44535bbafc1c9f9dd150a9e751c784aecf7b6682
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-10-22 18:41:41 -0400

    Registrar gasto: Error esperado de prueba cypress si no ingresamos nota

--- cypress/e2e/registrar-gasto.cy.js
+++ cypress/e2e/registrar-gasto.cy.js
@@ -45,4 +45,12 @@ describe("Registrar Gasto", () => {
     cy.get("#aniadir-gasto").click();
     cy.get("#gastos-div").should("contain", "5").and("contain", fechaActual).and("contain", "Gasto en transporte");
   });
+  it("Muestra mensaje de 'No hay notas disponibles' caso de solo ingresar el monto de gasto y la fecha", () => {
+    cy.visit("/");
+    cy.get("#monto-gasto").type(5);
+    cy.get("#fecha-gasto").type("2024-10-14");
+    cy.get("#nota-gasto").clear();
+    cy.get("#aniadir-gasto").click();
+    cy.get("#gastos-div").should("contain", "5").and("contain", "2024-10-14").and("contain", "No hay notas disponibles");
+  });
 });
