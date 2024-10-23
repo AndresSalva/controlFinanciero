46b81a72848846c54054c901260125f200f0b43d
Wendy Balderrama <balderramawendym@gmail.com>
2024-10-23 16:47:40 -0400

    Pruebas fallando del TDD Total Ingresos

--- src/control-financiero.spec.js
+++ src/control-financiero.spec.js
@@ -1,5 +1,6 @@
 import ControlFinanciero from './control-financiero.js'
 import Gasto from './gasto.js';
+import Ingreso from './ingreso.js';
 
 describe("Control financiero", () => {
     it("Registra el gasto en el control financiero", () => {
@@ -20,5 +21,26 @@ describe("Control financiero", () => {
 
         const totalGastitos= controlFinanciero1.verTotalGastitos();
         expect(totalGastitos).toEqual(470);
+    });
+
+    it("Registra el ingreso en el control financiero", () => {
+
+        const ingresito=new Ingreso;
+        const ingresito2=new Ingreso;
+        const ingresito3=new Ingreso;
+
+        const controlFinancieroIngreso=new ControlFinanciero;
+
+        ingresito.agregarMonto(10);
+        ingresito2.agregarMonto(30);
+        ingresito3.agregarMonto(100);
+
+        controlFinancieroIngreso.registrarIngreso(ingresito);
+        controlFinancieroIngreso.registrarIngreso(ingresito2);
+        controlFinancieroIngreso.registrarIngreso(ingresito3);
+
+        const totalIngresitos=controlFinancieroIngreso.verTotalIngresitos();
+        expect(totalIngresitos).toEqual(140);
+
     });
 });
