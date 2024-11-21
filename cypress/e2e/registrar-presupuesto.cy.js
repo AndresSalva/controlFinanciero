ed6b8078b97f3c1252ba3fecf32faa570e1eb250
AndresSalva <andres.salvatierra@ucb.edu.bo>
2024-11-20 21:37:17 -0400

    Añadiendo boton para mostrar el registro completo del presupuesto

--- cypress/e2e/registrar-presupuesto.cy.js
+++ cypress/e2e/registrar-presupuesto.cy.js
@@ -1,12 +1,14 @@
 describe("Registrar presupuesto", () => {
   it("Muestra monto de presupuesto ingresado", () => {
     cy.visit("/");
+    cy.get("#mostrar-form-presupuesto").click();
     cy.get("#monto-presupuesto").type(100);
     cy.get("#aniadir-presupuesto").click();
     cy.get("#presupuesto-div").should("contain", "100");
   });
   it("Muestra mensaje de 'MONTO VACIO!!!' si no se ingresa ningun monto", () => {
     cy.visit("/");
+    cy.get("#mostrar-form-presupuesto").click();
     cy.get("#aniadir-presupuesto").click();
     cy.get("#presupuesto-div").should("contain", "MONTO VACIO!!!");
   });
