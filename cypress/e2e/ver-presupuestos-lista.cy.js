8a1e923539f7ad3549fa3761b4550f957ab0d2c3
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-23 21:17:31 -0400

    Prueba cypress: Implementación de la lista de presupuestos con más presupuestos

--- cypress/e2e/ver-presupuestos-lista.cy.js
+++ cypress/e2e/ver-presupuestos-lista.cy.js
@@ -8,5 +8,20 @@ describe("Ver presupuestos", () => {
 
     cy.get("#totalPresupuesto-div").should("contain", "100").and("contain", "transporte");
   });
-  
+  it("Cuando se registre un presupuesto se mostrará la lista de presupuestos", () => {
+    cy.visit("/");
+    cy.get("#mostrar-form-presupuesto").click();
+    cy.get("#monto-presupuesto").type(100);
+    cy.get("#categoria-presupuesto").select("otros");
+    cy.get("#categoria-gasto-personalizada").type("universidad");
+    cy.get("#aniadir-presupuesto").click();
+
+    cy.get("#mostrar-form-presupuesto").click();
+    cy.get("#monto-presupuesto").type(35);
+    cy.get("#categoria-presupuesto").select("alimentacion");
+    cy.get("#aniadir-presupuesto").click();
+
+    cy.get("#totalPresupuesto-div").should("contain", "100").and("contain", "universidad");
+    cy.get("#totalPresupuesto-div").should("contain", "35").and("contain", "alimentacion");
   });
+});
