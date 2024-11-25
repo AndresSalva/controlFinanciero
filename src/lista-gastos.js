1d6c98387efd8026057c4934daef83ff43dae881
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-11-25 00:24:45 -0400

    Crear funcion editar gasto

--- src/lista-gastos.js
+++ src/lista-gastos.js
@@ -17,5 +17,13 @@ class ListaGastos {
         return "El gasto no se pudo eliminar, intentelo de nuevo";
       }
     }
+    editarGasto(index, nuevosDatos) {
+      const gasto = this.gastos[index];
+          gasto.agregarMonto(nuevosDatos.monto);
+          gasto.agregarFecha(nuevosDatos.fecha);
+          gasto.agregarNota(nuevosDatos.nota);
+          gasto.agregarCategoria(nuevosDatos.categoria);
+          return gasto; // Devuelve el gasto actualizado
+    }
   }
   export default ListaGastos;
