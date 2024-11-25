cda5c78bfd36246ba845061b0bf5ae2d17a689c8
Wendy Balderrama <balderramawendym@gmail.com>
2024-11-25 18:41:55 -0400

    Prueba cypress: Cada ingreso tiene un botón para poder eliminar un ingreso

--- cypress/e2e/ver-ingreses-lista.cy.js
+++ cypress/e2e/ver-ingreses-lista.cy.js
@@ -70,5 +70,54 @@ describe("Ver Ingresos", () => {
       cy.get("#totalIngresos-div").should("contain", "55");
     });
 
+    it("Cada ingreso tiene un botón para poder eliminar un ingreso", () => {
+      cy.visit("/");
+      cy.get("#mostrar-form-ingreso").click();
+      cy.get("#monto-ingreso").type(35);
+      cy.get("#fecha-ingreso").type('2024-12-25')
+      cy.get("#nota-ingreso").type('webada')
+     
+      cy.get("#categoria-ingreso").select("inversiones");
+      cy.get("#aniadir-ingreso").click();
+
+      cy.get("#ingreso-div").should("contain", "35");
+      cy.get(".eliminar-ingreso-btn").should('be.visible').click();
+      cy.get("#totalIngresos-div").should("not.contain", "otros");
+    });
+    /*it("Cada presupuesto tiene un botón para poder eliminar un presupuesto solo si confirma", () => {
+      cy.visit("/");
+      cy.get("#mostrar-form-presupuesto").click();
+      cy.get("#monto-presupuesto").type(35);
+      cy.get("#categoria-presupuesto").select("alimentacion");
+      cy.get("#aniadir-presupuesto").click();
+      cy.get("#totalPresupuesto-div").should("contain", "35").and("contain", "alimentacion");
+      cy.window().then((window) => {
+        cy.stub(window, 'confirm').returns(true);
+      });
+      cy.get(".eliminar-presupuesto-btn").should('be.visible').click();
+      cy.get("#totalPresupuesto-div").should("not.contain", "35");
+      cy.get("#totalPresupuesto-div").should("not.contain", "alimentacion");
+    });
+    it("No debería eliminar un presupuesto si el usuario no confirma", () => {
+      cy.visit("/");
+    
+      cy.get("#mostrar-form-ingreso").click();
+      cy.get("#monto-ingreso").type(55);
+      cy.get("#categoria-ingreso").select("alimentacion");
+      cy.get("#aniadiro").click();
+    
+      cy.get("#totalPresupuesto-div").should("contain", "55").and("contain", "alimentacion");
+    
+      cy.window().then((window) => {
+        cy.stub(window, 'confirm').returns(false);
+      });
+    
+      cy.get(".eliminar-presupuesto-btn").should('be.visible').click();
+    
+      cy.get("#totalPresupuesto-div").should("contain", "55");
+      cy.get("#totalPresupuesto-div").should("contain", "alimentacion");
+    });
+*/
+
   });
   
