ea9d3a6fd5bf197b116bfa2759e9558aed3bcd5a
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-25 21:22:44 -0400

    Prueba Cypress: Informe de presupuesto ingresando una categoria que no existe

--- cypress/e2e/InformePresupuesto.cy.js
+++ cypress/e2e/InformePresupuesto.cy.js
@@ -39,6 +39,33 @@ describe("Ver Presupuestos y Gastos", () => {
           .and("contain", "Gasto: 200")
           .and("contain", "Total: 800"); 
     });
+    it("Muestra un presupuesto y un gasto en el informe con categoria personalizada", () => {
+        cy.visit("/"); 
+    
+        cy.get("#mostrar-form-presupuesto").click();
+        cy.get("#monto-presupuesto").type(100);
+        cy.get("#categoria-presupuesto").select("otros");
+        cy.get("#categoria-gasto-personalizada").type("universidad");
+        cy.get("#aniadir-presupuesto").click();
+    
+        cy.get("#informePresupuesto-div")
+          .should("contain", "universidad")
+          .and("contain", "Presupuesto: 100")
+          .and("contain", "Gasto: 0")
+          .and("contain", "Total: 100");
+    
+        cy.get("#mostrar-form-btn").click();
+        cy.get("#monto-gasto").type(10);
+        cy.get("#categoria-gasto").select("otros");
+        cy.get("#categoria-gastos").type("universidad");
+        cy.get("#aniadir-gasto").click(); 
+    
+        cy.get("#informePresupuesto-div")
+          .should("contain", "universidad")
+          .and("contain", "Presupuesto: 100")
+          .and("contain", "Gasto: 10")
+          .and("contain", "Total: 90"); 
+    });
 
 
   });
