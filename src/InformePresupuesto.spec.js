235ff186488278e346b240b7e285fb20067b5e51
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-25 21:19:47 -0400

    Prueba Jest: Informe de presupuesto ingresando una categoria que no existe

--- src/InformePresupuesto.spec.js
+++ src/InformePresupuesto.spec.js
@@ -11,4 +11,16 @@ describe('Informe Presupuesto', () => {
           { nombre: 'Alimentos', presupuesto: 1000, gasto: 200 }
         ]);
     });
+    it('Debería actualizar una categoría existente correctamente', () => {
+        const informe = new informePresupuesto();
+    
+        informe.agregarOActualizarCategoria('Alimentos', 1000, 200);
+    
+        informe.agregarOActualizarCategoria('Alimentos', 500, 100);
+    
+        const categorias = informe.obtenerCategorias();
+        expect(categorias).toEqual([
+          { nombre: 'Alimentos', presupuesto: 1500, gasto: 300 } 
+        ]);
+    });
 });
